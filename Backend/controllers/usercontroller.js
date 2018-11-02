8d925adce89b42c8ac224186c3eeb317939abb6b
scottjstewart <scottstewart965@gmail.com>
2018-11-02 11:09:32 -0400

    new oull for q

--- Backend/controllers/usercontroller.js
+++ Backend/controllers/usercontroller.js
@@ -13,7 +13,6 @@ module.exports = (app, db) => {
             model: db.buzzs,
             include: [
               {
-
                 userId: user.id,
                 firstName: user.firstName,
                 lastName: user.lastName,
@@ -22,7 +21,6 @@ module.exports = (app, db) => {
                 userName: user.userName,
                 pet: user.pet,
                 buzzs: user.buzzs.map(buzz => {
-    
                   //tidy up the post data
                   return Object.assign(
                     {},
@@ -34,21 +32,19 @@ module.exports = (app, db) => {
                       funFactor: buzz.funFactor,
                       details: buzz.details,
                       comments: buzz.comments.map(comment => {
-    
                         //tidy up the comment data
                         return Object.assign(
                           {},
                           {
                             commentId: comment.id,
                             userId: comment.userId,
-                            commenter: comment.commenterUserName,
+                            commenter: comment.commenterUserName
                           }
-                        )
+                        );
                       })
                     }
-                    )
+                  );
                 })
-              model: db.comments
               }
             ]
           }
@@ -135,115 +131,119 @@ module.exports = (app, db) => {
   });
 
   app.post("/user/login", (req, res) => {
-    user.findOne({ where: { userName: req.body.userName } }).then(
-      user => {
-        if (user) {
-          bcrypt.compare(req.body.password, user.password, (err, matches) => {
-            if (matches) {
-              let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-                expiresIn: 60 * 60 * 24
-              });
-              let resUser = {
-                userName: newUser.userName,
-                firstName: newUser.firstName,
-                lastName: newUser.lastName,
-                email: newUser.email,
-                pet: newUser.pet,
-                id: newUser.userId
-              }
-              res.json({
+    user.findOne({ where: { userName: req.body.userName } }).then(user => {
+      if (user) {
+        bcrypt.compare(req.body.password, user.password, (err, matches) => {
+          if (matches) {
+            let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+              expiresIn: 60 * 60 * 24
+            });
+            let resUser = {
+              userName: newUser.userName,
+              firstName: newUser.firstName,
+              lastName: newUser.lastName,
+              email: newUser.email,
+              pet: newUser.pet,
+              id: newUser.userId
+            };
+            res.json({
+              user: resUser,
+              auth: true,
+              message: "User successfuly created",
+              sessionToken: token
+            });
+          }
+          createError = err => res.send(500, err.message);
+        });
+      }
+    });
+
+    app.post("/user/login", (req, res) => {
+      user.findOne({ where: { userName: req.body.userName } }).then(
+        user => {
+          if (user) {
+            bcrypt.compare(req.body.password, user.password, (err, matches) => {
+              if (matches) {
+                let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+                  expiresIn: 60 * 60 * 24
+                });
+                let resUser = {
+                  userName: user.userName,
+                  firstName: user.firstName,
+                  lastName: user.lastName,
+                  email: user.email,
+                  pet: user.pet,
+                  id: user.userId
+                };
+                res.json({
                   user: resUser,
                   auth: true,
-                  message: 'User successfuly created',
+                  message: "Success!",
                   sessionToken: token
-              })
-          },
-          createError = err => res.send(500, err.message)
-        )
-      });
-
-      app.post('/user/login', (req, res) => {
-        user.findOne({where: {userName: req.body.userName } } )
-          .then(
-            user => {
-              if (user) {
-                bcrypt.compare(req.body.password, user.password, (err, matches) => {
-                  if (matches) {
-                      let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 })
-                      let resUser = {
-                        userName: user.userName,
-                        firstName: user.firstName,
-                        lastName: user.lastName,
-                        email: user.email,
-                        pet: user.pet,
-                        id: user.userId
-                      }
-                      res.json({
-                          user: resUser,
-                          auth: true,
-                          message: 'Success!',
-                          sessionToken: token
-                      })
-                  } else {
-                      res.status(502).send({ error: 'bad gateway' })
-                  }
-              })
+                });
+              } else {
+                res.status(502).send({ error: "bad gateway" });
+              }
+            });
           } else {
-              res.status(500).send({ error: 'failed to authenticate' })
+            res.status(500).send({ error: "failed to authenticate" });
           }
-      },
-      err => res.status(501).send({ error: "failed to process" })
-    );
-  });
+        },
+        err => res.status(501).send({ error: "failed to process" })
+      );
+    });
 
-  app.get("/user/get", validateSession, (req, res) => {
-    user
-      .findAll()
-      .then(user => res.status(200).json(user))
-      .catch(err => res.status(500).json({ error: err }));
-  });
-
-  app.delete("/user/delete", validateSession, (req, res) => {
-    if (!req.errors) {
+    app.get("/user/get", validateSession, (req, res) => {
       user
-        .destroy({ where: { id: req.user.id } })
+        .findAll()
         .then(user => res.status(200).json(user))
-        .catch(err => res.json(req.error));
-    } else {
-      res.status(500).json(req.error);
-    }
-  });
+        .catch(err => res.status(500).json({ error: err }));
+    });
 
-  app.put("/user/update", validateSession, (req, res) => {
-    user.findOne({ where: { id: req.user.id } }).then(user => {
-      nUser = {
-        password:
-          req.body.password && req.body.password !== ""
-            ? bcrypt.hashSync(req.body.password, 10)
-            : user.password,
-        firstName:
-          req.body.firstName && req.body.firstName !== ""
-            ? req.body.firstName
-            : user.firstName,
-        lastName:
-          req.body.lastName && req.body.lastName !== ""
-            ? req.body.lastName
-            : user.lastName,
-        email:
-          req.body.email && req.body.email !== "" ? req.body.email : user.email,
-        userName:
-          req.body.userName && req.body.userName !== ""
-            ? req.body.userName
-            : user.userName
-      };
+    app.delete("/user/delete", validateSession, (req, res) => {
       if (!req.errors) {
         user
-          .update(nUser, { where: { id: user.id } })
-          .then(userN => res.status(200).json(userN))
-          .catch(error => res.json(error));
+          .destroy({ where: { id: req.user.id } })
+          .then(user => res.status(200).json(user))
+          .catch(err => res.json(req.error));
       } else {
         res.status(500).json(req.error);
       }
+    });
+
+    app.put("/user/update", validateSession, (req, res) => {
+      user.findOne({ where: { id: req.user.id } }).then(user => {
+        nUser = {
+          password:
+            req.body.password && req.body.password !== ""
+              ? bcrypt.hashSync(req.body.password, 10)
+              : user.password,
+          firstName:
+            req.body.firstName && req.body.firstName !== ""
+              ? req.body.firstName
+              : user.firstName,
+          lastName:
+            req.body.lastName && req.body.lastName !== ""
+              ? req.body.lastName
+              : user.lastName,
+          email:
+            req.body.email && req.body.email !== ""
+              ? req.body.email
+              : user.email,
+          userName:
+            req.body.userName && req.body.userName !== ""
+              ? req.body.userName
+              : user.userName
+        };
+        if (!req.errors) {
+          user
+            .update(nUser, { where: { id: user.id } })
+            .then(userN => res.status(200).json(userN))
+            .catch(error => res.json(error));
+        } else {
+          res.status(500).json(req.error);
+        }
+      });
     });
   });
 };
