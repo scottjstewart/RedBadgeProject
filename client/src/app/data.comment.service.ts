ac98f0d986dd65baa416d511c2e6f7059f5e6516
scottjstewart <scottstewart965@gmail.com>
2018-11-06 11:17:16 -0500

    added service functions and imported data service in the comment component. Everything is ready to link up with buttons.

--- client/src/app/data.comment.service.ts
+++ client/src/app/data.comment.service.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { tap } from "rxjs/operators";
+import { AuthUserService } from "./data.auth-user.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
-export class Data.CommentService {
+export class DataCommentService {
+  newComment: Object;
 
-  constructor() { }
+  constructor(private http: HttpClient, private auth: AuthUserService) {}
+
+  getComments() {
+    return this.http
+      .get("http://localhost:3000/comment/get")
+      .pipe(tap(data => console.log(data)));
+  }
+
+  getOwnComment(userId, commentId) {
+    return this.http.get("http://localhost:3000/comment/own" + commentId);
+  }
+
+  createComment(comment) {
+    return this.http.post("http://localhost:3000/comment/create", comment);
+  }
+
+  updateComment(commentId, newComment) {
+    return this.http.put(
+      "http://localhost:3000/comment/update" + commentId,
+      newComment
+    );
+  }
+
+  deleteComment(commentId) {
+    return this.http.delete("http://localhost:3000/comment/delete" + commentId);
+  }
+
+  submit() {
+    return console.log("clicked");
+  }
 }
