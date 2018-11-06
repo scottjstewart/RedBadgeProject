9a62be8f48118cff2ec4c81e23a1c4ea0f72064a
scottjstewart <scottstewart965@gmail.com>
2018-11-06 14:13:23 -0500

    akhbfkja

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,7 @@ const Client = require("./controllers/clientcontroller");
 const Com = require("./controllers/commentcontroller");
 const router = require("./controllers/router");
 
-db.sequelize.sync({ force: true });
+db.sequelize.sync();
 
 app.use(bodyParser.json());
 app.use(require("./middleware/header"));
@@ -25,10 +25,10 @@ app.use(require("./middleware/validate-session"));
 
 app.use("/client", Client);
 
-app.use("/api/test", function (req, res) {
-    res.send("Looks like this is working...because we are awesome");
+app.use("/api/test", function(req, res) {
+  res.send("Looks like this is working...because we are awesome");
 });
 
-app.listen(3000, function () {
-    console.log("app listening on port 3000");
+app.listen(3000, function() {
+  console.log("app listening on port 3000");
 });
